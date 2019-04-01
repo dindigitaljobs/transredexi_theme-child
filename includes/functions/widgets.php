03ba351d5bd68af9d5f28cc3d14364c7520606e6
Kevin Oliveira <kevin@dindigital.com>
2019-04-01 14:37:45 -0300

    Fix tag limit array

--- includes/functions/widgets.php
+++ includes/functions/widgets.php
@@ -1,12 +1,16 @@
 <?php
+
 //Register tag cloud filter callback
 add_filter('widget_tag_cloud_args', 'tag_widget_limit');
  
 //Limit number of tags inside widget
-function tag_widget_limit(){
-  $args = array(
-    'number' => 10
-  );
-  return $args;
+function tag_widget_limit($args){
+ 
+ //Check if taxonomy option inside widget is set to tags
+ if(isset($args['taxonomy']) && $args['taxonomy'] == 'post_tag'){
+  $args['number'] = 10; //Limit number of tags
+ }
+ 
+ return $args;
 }
  
