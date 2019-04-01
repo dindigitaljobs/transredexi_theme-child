ef6aeff31f103873afea45d50215c2400a601dcf
Kevin Oliveira <kevin@dindigital.com>
2019-04-01 13:55:49 -0300

    Remove widget limit

--- includes/functions/widgets.php
+++
@@ -1,8 +0,0 @@
-function tag_widget_limit($args){ 
-  if(isset($args['taxonomy']) && $args['taxonomy'] == 'post_tag'){
-    $args['number'] = 15; //número desejado
-  }
-
-  return $args;
-}
-add_filter('widget_tag_cloud_args', 'tag_widget_limit');
