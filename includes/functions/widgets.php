760a2e9a8401c6d143c722b6d24e9d8d515f1af1
Kevin Oliveira <kevin@dindigital.com>
2019-04-01 14:32:07 -0300

    Add widget limit tags

---
+++ includes/functions/widgets.php
@@ -0,0 +1,14 @@
+//Register tag cloud filter callback
+add_filter('widget_tag_cloud_args', 'tag_widget_limit');
+ 
+//Limit number of tags inside widget
+function tag_widget_limit($args){
+ 
+ //Check if taxonomy option inside widget is set to tags
+ if(isset($args['taxonomy']) && $args['taxonomy'] == 'post_tag'){
+  $args['number'] = 10; //Limit number of tags
+ }
+ 
+ return $args;
+}
+ 
